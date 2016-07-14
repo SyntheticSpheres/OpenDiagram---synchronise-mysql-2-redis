69e01a3662d43221000dc7c61fb55a0787497228
Vladimir Baranek <vladimir.baranek@syntheticspheres.com>
2016-07-14 14:03:49 -0700

    added activity modes,play modes and authors

--- model/synchro-map.js
+++ model/synchro-map.js
@@ -30,14 +30,20 @@ function redis_entity(res, dbRedis) {
       case 1003:
       redis.hmset("diagram type:"+row.eId,"name", row.entity);
         break;
-      case 1004:
-      redis.hmset("file extension:"+row.eId,"name", row.entity);
-        break;
       case 1006:
       redis.hmset("audience:"+row.eId,"name", row.entity);
         break;
       case 1007:
       redis.hmset("collection:"+row.eId,"name", row.entity);
+        break;
+      case 1008:
+      redis.hmset("language:"+row.eId,"name", row.entity);
+        break;
+      case 1010:
+      redis.hmset("play mode:"+row.eId,"name", row.entity);
+        break;
+      case 1011:
+      redis.hmset("activity mode:"+row.eId,"name", row.entity);
         break;
     }
     redis.sadd("semantic entities", row.eId, redis.print);
@@ -45,6 +51,18 @@ function redis_entity(res, dbRedis) {
   synchronizationTasksDone(redis);
   utilities.log({SYNC : "redis_entity", status : "synchronized " + res.length.toString() + " records"}, 'f');
 }
+
+// Synchronization: Authors (Redis SET + Hashes)
+function redis_author(res, dbRedis) {
+  var redis = dbRedis;
+  res.forEach(function(row) {
+    redis.hmset("author:"+row.aId, "first name", row.fName,"last name", row.lName, "address", row.address);
+    redis.sadd("authors", row.aId, redis.print);
+  });
+  synchronizationTasksDone(redis);
+  utilities.log({SYNC : "redis_authors", status : "synchronized " + res.length.toString() + " records"}, 'f');
+}
+
 
 function synchronizationTasksDone(redis) {
   redis.incr("synchronization_tasks");
@@ -58,5 +76,8 @@ module.exports = [
   },{
     query : 'SELECT `Entity ID` AS "eId", `Entity Name` AS "entity" , `Semantic ID` AS "sId" FROM metasystem_entity ORDER BY `Semantic ID` , `Entity Name`;',
     callback : redis_entity
+  },{
+    query : 'SELECT `Composition ID` AS "aId", `Composition First Name` AS "fName" , `Composition Last Name` AS "lName" , `Composition Address` AS "address" FROM pattern_composition ORDER BY `Composition ID` , `Composition Last Name` , `Composition First Name`;',
+    callback : redis_author
   }
 ];
